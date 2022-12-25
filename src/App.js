7135ca8a1508786f4ce35ebdea40223c98a72696
maximilian99-ai <maximilian99@icloud.com>
2022-12-25 22:48:26 +0900

    snow effect

--- src/App.js
+++ src/App.js
@@ -7,14 +7,37 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 
 function App() {
+  const MIN_DURATION = 10;
+  const body = document.querySelector("body");
+
+  function makeSnowFlake() {
+    const snowFlake = document.createElement("div");
+    const delay = Math.random() * 10;
+    const initialOpacity = Math.random();
+    const duration = Math.random() * 20 + MIN_DURATION;
+
+    snowFlake.classList.add("snowFlake");
+    snowFlake.style.left = `${Math.random() * window.screen.width}px`;
+    snowFlake.style.animationDelay = `${delay}s`;
+    snowFlake.style.opacity = initialOpacity;
+    snowFlake.style.animation = `fall ${duration}s linear`;
+
+    body.appendChild(snowFlake);
+  }
+
+  makeSnowFlake();
+
+  for (let index = 0; index < 50; index++) {
+    setTimeout(makeSnowFlake, 500 * index);
+  }
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" exact={ true } element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/movie-detail" element={<Detail />} />
-      </Routes>
+        <Routes>
+          <Route path="/" exact={ true } element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/movie-detail" element={<Detail />} />
+        </Routes>
     </BrowserRouter>
   );
 }
